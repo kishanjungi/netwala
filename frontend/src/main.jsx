d6d7f6d8d759bb35f5aca8115d25fe973da8a142
kishanjungi <kishanjungi5@gmail.com>
2026-01-31 22:09:43 +0530

    update main .jsx file use hashrouter insted of browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ShopContextProvider from './context/ShopContext.jsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-    <BrowserRouter>
+    <HashRouter>
       <ShopContextProvider>
         <App />
       </ShopContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </GoogleOAuthProvider>
 );
