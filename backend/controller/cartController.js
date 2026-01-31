5fbcb20638bfd6b99a29707df7ec3c0ad0a61a85
kishanjungi <kishanjungi5@gmail.com>
2026-01-31 14:21:05 +0530

    cart controller updated

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -1,84 +1,66 @@
-import userModel from "../models/userModel.js";
+   import userModel from '../models/userModel.js';
 
-const addToCart = async (req, res) => {
-  try {
-    const { itemId, size } = req.body;
-    const userId = req.userId; 
+   const addToCart = async (req, res) =>{
+      try{
+         const {userId,itemId,size}=req.body;
+         
+         const userData= await userModel.findById(userId);
+         let cartData=await userData.cartData;
+         
+         if(cartData[itemId]){
+            if(cartData[itemId][size]){
+               cartData[itemId][size]+=1;
 
-    if (!userId) {
-      return res.status(401).json({
-        success: false,
-        message: "Please login to add items to cart"
-      });
-    }
+            }else{
+               cartData[itemId][size]=1;
+            }
+         }else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1;
+         }
 
-    const userData = await userModel.findById(userId);
-    let cartData = userData.cartData || {};
+         await userModel.findByIdAndUpdate(userId,{cartData});
+         res.json({success:true, message:"Added to Cart"})
+      }catch(error){
+      console.log(error);
+      res.json({success:false,message:error.message})
+      }
+   }
 
-    if (cartData[itemId]) {
-      cartData[itemId][size] = (cartData[itemId][size] || 0) + 1;
-    } else {
-      cartData[itemId] = {};
-      cartData[itemId][size] = 1;
-    }
+   const updateCart = async(req, res) => {
+      try{
+         const {userId,itemId,size,quantity}=req.body;
+         
+         const userData= await userModel.findById(userId);
+         let cartData=await userData.cartData;
 
-    await userModel.findByIdAndUpdate(userId, { cartData });
+         cartData[itemId][size]=quantity;
 
-    res.json({ success: true, message: "Added to Cart" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: error.message });
-  }
-};
+            await userModel.findByIdAndUpdate(userId,{cartData});
+         res.json({success:true, message:"Cart Updated"})   
 
-const updateCart = async (req, res) => {
-  try {
-    const { itemId, size, quantity } = req.body;
-    const userId = req.userId;
 
-    if (!userId) {
-      return res.status(401).json({
-        success: false,
-        message: "Please login to update cart"
-      });
-    }
 
-    const userData = await userModel.findById(userId);
-    let cartData = userData.cartData || {};
+      }catch(error){
+         console.log(error);
+      res.json({success:false,message:error.message})
+      }
 
-    if (cartData[itemId]) {
-      cartData[itemId][size] = quantity;
-    }
+   }
+   const getUserCart = async(req, res) =>{
+      try{
+         
+         const {userId}=req.body;
+         
+         const userData= await userModel.findById(userId);
+         let cartData=await userData.cartData;
 
-    await userModel.findByIdAndUpdate(userId, { cartData });
-
-    res.json({ success: true, message: "Cart Updated" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-const getUserCart = async (req, res) => {
-  try {
-    const userId = req.userId;
-
-    if (!userId) {
-      return res.status(401).json({
-        success: false,
-        message: "Please login to view cart"
-      });
-    }
-
-    const userData = await userModel.findById(userId);
-    let cartData = userData.cartData || {};
-
-    res.json({ success: true, cartData });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: error.message });
-  }
-};
+         res.json({success:true,cartData})
+      }catch(error){
+         console.log(error);
+      res.json({success:false,message:error.message})
+      }
+   }
 
 
    export { addToCart, updateCart, getUserCart }
